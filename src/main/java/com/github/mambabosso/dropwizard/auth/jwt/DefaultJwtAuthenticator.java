0707ef1bcb1817dc25a4e5419949615f344d49ed
Luis Weich <mambabosso@gmail.com>
2020-04-23 21:50:03 +0200

    Update

--- src/main/java/com/github/mambabosso/dropwizard/auth/jwt/DefaultJwtAuthenticator.java
+++ src/main/java/com/github/mambabosso/dropwizard/auth/jwt/DefaultJwtAuthenticator.java
@@ -5,15 +5,8 @@ import java.security.Principal;
 
 public final class DefaultJwtAuthenticator<T extends Principal & Serializable> extends JwtAuthenticator<T> {
 
-    private final JwtHandler<T> handler;
-
     private DefaultJwtAuthenticator(final JwtHandler<T> handler) {
         super(handler);
-        this.handler = handler;
-    }
-
-    public JwtHandler<T> getJwtHandler() {
-        return handler;
     }
 
     public static <T extends Principal & Serializable> DefaultJwtAuthenticator<T> create(final JwtConfiguration configuration, final Class<T> tClass) {
